8807920d44748693a2d03c04d8e2cf6da8dd28c4
kinokus <aleks.kinokus@gmail.com>
2020-03-11 11:09:14 +0200

    app main modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SecureComponent } from './secure/secure.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SecureComponent,
+    CategoriesComponent,
+    MoviesComponent,
+    MovieCardComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
